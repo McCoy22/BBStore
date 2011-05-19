9ee76aeab968ce7d9248b5b6a234c68bdd74fed6
MoA <meyer.otte@gmail.com>
2011-05-19 12:33:08 +0200

    Organized Imports

--- src/bb/servlets/ShowCart.java
+++ src/bb/servlets/ShowCart.java
@@ -2,7 +2,6 @@ package bb.servlets;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
